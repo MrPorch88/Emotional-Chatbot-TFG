2f441cb1658294d323e1f4c6e933b5a7a4886582
Jorge Moreno <jorge.velez.moreno@gmail.com>
2021-03-21 20:44:11 +0100

    Arreglo gitIgnore y agregados de codigo

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000; // Definimos el puerto para Heroku y local
 
 app.get('/', (req, res)=>{
     res.send({'mensaje':'prueba'});
 });
 
-app.listen(5000);
+app.listen(PORT);
 
 
