bc583ec7318aebefe4604938654829f7822d079a
Jorge Moreno <jorge.velez.moreno@gmail.com>
2021-04-11 18:26:56 +0200

    Hotfix
    Hotfix para parsear PrivateKey

--- config/prodKeys.js
+++ config/prodKeys.js
@@ -2,6 +2,6 @@ module.exports = {
     googleProjectID: process.env.GOOGLE_PROJECT_ID,
     dialogFlowSessionID: process.env.DIALOG_FLOW_SESSION_ID,
     dialogFlowSessionLanguageCode: process.env.DIALOG_FLOW_SESSION_LANGUAGE_CODE,
-    googlePrivate_key: process.env.GOOGLE_PRIVATE_KEY,
+    googlePrivate_key: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
     googleClient_email: process.env.GOOGLE_CLIENT_EMAIL
-}
+};
