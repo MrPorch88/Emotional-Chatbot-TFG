ee4ca6c65c1ca2122b6d3aedb660dd6d837ebcf4
Jorge Moreno <jorge.velez.moreno@gmail.com>
2021-05-12 11:03:13 +0200

    Aceptación de cookies
    Agregado banner para avisar y aceptar el uso de cookies en la aplicación web.

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -5,10 +5,12 @@ import React from 'react';
 import Footer from '../Footer';
 import Logo from './chatbotlogo.png';
 import Chatbot from '../chatbot/Chatbot';
+import CookieConsent from "react-cookie-consent";
 
 function Landing() {
     return(
         <div className="Landing" style={{ textAlign: 'center'}}  alignitems="center">
+            <CookieConsent>This website uses cookies to stablish a unique ID for each user enhancing the overall experience.</CookieConsent>
             <img src={Logo} alt="logo" height='200'/>
             <div>
                 <Chatbot/>
